c42c7b6357b0795ecf5b6f33b3f25790524f2724
Sehajdeep Singh <ssingh28_be24@thapar.edu>
2025-12-12 19:29:38 +0530

    Add files via upload

---
+++ Day 17 Projects/Quiz-Game/data.py
@@ -0,0 +1,13 @@
+Question_data = [
+    {"text": "A slug's blood is green", "answer": True},
+    {"text": "Lightning is hotter than the surface of the Sun.", "answer": True},
+    {"text": "Humans have four lungs.", "answer": False},
+    {"text": "Octopuses have three hearts.", "answer": True},
+    {"text": "Bananas grow on trees.", "answer": False},
+    {"text": "The fastest land animal is the cheetah.", "answer": True},
+    {"text": "Water boils at 100°C at sea level.", "answer": True},
+    {"text": "Goldfish only have a memory of three seconds.", "answer": False},
+    {"text": "Sharks are mammals.", "answer": False},
+    {"text": "Venus is the hottest planet in our solar system.", "answer": True},
+    {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": False}
+]
